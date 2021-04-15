5e8d0609bdeb34f556f3f5d786d817c50e99cd74
AndresEduardoGarciaSosa <andreseduardo@ufps.edu.com>
2021-04-15 08:21:12 -0500

    commit4

--- js/proceso.js
+++ js/proceso.js
@@ -13,10 +13,22 @@ async function leerJSON(url) {
 function cargarNoticias()
 {
 	var url="https://carlosreneas.github.io/endpoints/noticias.json";
+	var url1="https://carlosreneas.github.io/endpoints/categoria_deporte.json";
+	var url2="https://carlosreneas.github.io/endpoints/categoria_tecnologia.json";
 
 	leerJSON(url).then(datos=>{
 	console.log(datos);
 	mostrarNoticia(datos);
+	})
+
+	leerJSON(url1).then(datos1=>{
+	console.log(datos1);
+	mostrarDeportes(datos1);
+	})
+
+	leerJSON(url1).then(datos2=>{
+	console.log(datos2);
+	mostrarTecnologia(datos2);
 	})
 }
 
@@ -41,11 +53,33 @@ function mostrarDeportes(datos)
 
 	for(let i=0;i<datos.length;i++)
 	{
-		div+="<div id='rta"+i+"'> </div>";
+		div+="<div id='deporte"+i+"'> </div>";
 	}
+	document.getElementById("deportes").innerHTML=div;
+
+	var title="";
+	for(let j=0;j<datos.length;j++)
+	{
+		title="<a href='#''>"+datos[j].titulo+"</a>";
+		document.getElementById("deporte"+j).innerHTML=title;
+	}
+}
+
+function mostrarTecnologia(datos)
+{
+	var div="";
 
 	for(let i=0;i<datos.length;i++)
 	{
-		title="<p>"++"</p>"
+		div+="<div id='tecnologia"+i+"'> </div>";
 	}
+	document.getElementById("tecnologias").innerHTML=div;
+
+	var title="";
+	for(let j=0;j<datos.length;j++)
+	{
+		title="<a href='#''>"+datos[j].titulo+"</a>";
+		document.getElementById("tecnologia"+j).innerHTML=title;
+	}
+
 }
